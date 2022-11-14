e9f72f82e6288204ba79b603afc838ba9b249e6b
jonh.carvalho <jonh.carvalho@prof.infnet.edu.br>
2022-11-14 10:04:36 -0300

    Adicionado ViewPager

--- app/src/main/java/br/edu/infnet/apppb/MainActivity.kt
+++ app/src/main/java/br/edu/infnet/apppb/MainActivity.kt
@@ -2,10 +2,32 @@ package br.edu.infnet.apppb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewPager: ViewPager
+    lateinit var viewPagerAdapter: ViewPagerAdapter
+    lateinit var imageList: List<Int>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewPager = findViewById(R.id.idViewPager)
+
+        imageList = ArrayList<Int>()
+        imageList = imageList + R.drawable.ale
+        imageList = imageList + R.drawable.arg
+        imageList = imageList + R.drawable.bratrans
+
+        viewPagerAdapter = ViewPagerAdapter(this@MainActivity, imageList)
+
+        viewPager.adapter = viewPagerAdapter
+
+
+
+
     }
 }
