afe3cbe34e34bd2a97980a0b3b254b967b1fa43c
jonh.carvalho <jonh.carvalho@prof.infnet.edu.br>
2022-11-14 12:34:46 -0300

    Adicionado PBAuth sem funcionar

---
+++ app/pbauth/src/main/java/br/edu/infnet/pbauth/LoginActivity.kt
@@ -0,0 +1,57 @@
+package br.edu.infnet.pbauth
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var tvRedirectSignUp: TextView
+    lateinit var etEmail: EditText
+    private lateinit var etPass: EditText
+    lateinit var btnLogin: Button
+
+    lateinit var  auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // View Binding
+        tvRedirectSignUp = findViewById(R.id.tvRedirectSignUp)
+        btnLogin = findViewById(R.id.btnLogin)
+        etEmail = findViewById(R.id.etEmailAddress)
+        etPass = findViewById(R.id.etPassword)
+
+        auth = FirebaseAuth.getInstance()
+
+        btnLogin.setOnClickListener{
+            login()
+        }
+
+        tvRedirectSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+            // using finish() to end the activity
+            finish()
+        }
+}
+
+    private fun login() {
+        val email = etEmail.text.toString()
+        val pass = etPass.text.toString()
+
+        auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Successfully LoggedIn", Toast.LENGTH_SHORT).show()
+            }else
+                Toast.makeText(this, "Log In failed ", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
--- app/src/main/java/br/edu/infnet/apppb/MainActivity.kt
+++ app/src/main/java/br/edu/infnet/apppb/MainActivity.kt
@@ -1,33 +1,54 @@
 package br.edu.infnet.apppb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.viewpager.widget.ViewPager
 
 class MainActivity : AppCompatActivity() {
 
+    // on below line we are creating variable for view pager,
+    // viewpager adapter and the image list.
     lateinit var viewPager: ViewPager
     lateinit var viewPagerAdapter: ViewPagerAdapter
     lateinit var imageList: List<Int>
 
+    lateinit var skip: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        skip = findViewById<View>(R.id.btn_skip) as Button
+
+        // initializing variables
+        // of below line with their id.
         viewPager = findViewById(R.id.idViewPager)
 
+        // on below line we are initializing
+        // our image list and adding data to it.
         imageList = ArrayList<Int>()
         imageList = imageList + R.drawable.ale
-        imageList = imageList + R.drawable.arg
         imageList = imageList + R.drawable.bratrans
 
+
+        // on below line we are initializing our view
+        // pager adapter and adding image list to it.
         viewPagerAdapter = ViewPagerAdapter(this@MainActivity, imageList)
 
+        // on below line we are setting
+        // adapter to our view pager.
         viewPager.adapter = viewPagerAdapter
 
 
+        //For the next and previous buttons
 
-
+        skip.setOnClickListener(View.OnClickListener { view: View? ->
+            val i = Intent(this@MainActivity, Home::class.java)
+            startActivity(i)
+            finish()
+        })
     }
 }
