2d5466b4b1e12b4c46033532f55635a70caede70
jonh.carvalho <jonh.carvalho@prof.infnet.edu.br>
2022-11-23 12:14:14 -0300

    AdMob Interstitial

--- app/src/main/java/br/edu/infnet/apppb/HomeActivity.kt
+++ app/src/main/java/br/edu/infnet/apppb/HomeActivity.kt
@@ -1,11 +1,17 @@
 package br.edu.infnet.apppb
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.infnet.apppb.databinding.ActivityHomeBinding
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -15,6 +21,7 @@ class HomeActivity : AppCompatActivity() {
 
 
     private lateinit var binding : ActivityHomeBinding
+    private var anuncioInter: InterstitialAd? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +36,50 @@ class HomeActivity : AppCompatActivity() {
        val adRequest = AdRequest.Builder().build()
        binding.adView.loadAd(adRequest)
 
+        requestNewInterstitial()
+
+        //requestReward()
+
         getData()
+    }
+
+
+    private fun requestNewInterstitial() {
+        val adRequest = AdRequest.Builder().build()
+        InterstitialAd.load(this, "ca-app-pub-3940256099942544/1033173712", adRequest,
+            object : InterstitialAdLoadCallback() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    Log.d("INTERSTTIAL", adError.message)
+                    anuncioInter =null
+                }
+
+                override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                    Log.d("INTERSTITIAL", "Ad was loaded.")
+                    anuncioInter = interstitialAd
+                    configureFullScreen()
+                }
+
+
+            }
+        )
+    }
+
+    private fun configureFullScreen() {
+        anuncioInter?.fullScreenContentCallback = createFullScreenCalback("INTERSTITIAL") {
+            anuncioInter = null
+        }
+    }
+
+    private fun createFullScreenCalback(TAG: String, funcao: () -> Unit ): FullScreenContentCallback
+            = object : FullScreenContentCallback() {
+        override fun onAdDismissedFullScreenContent() {
+            Log.d(TAG, "Ad was dismissed.")
+        }
+            }
+
+
+    private fun requestReward() {
+
     }
 
     fun getData() {
@@ -52,4 +102,17 @@ class HomeActivity : AppCompatActivity() {
             }
         })
     }
+
+    fun interClick(view: View) {
+        if(anuncioInter != null){
+            anuncioInter?.show(this)
+        }
+        else{
+            Log.d("INTERSTITIAL", "The interstitial ad wasn't ready yet.")
+        }
+    }
+
+    fun rewardClick(view: View) {}
+
+
 }
